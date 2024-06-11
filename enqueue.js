4e0bf303f379242f47ad7b28c7669738f5c6ba46
Xom <xom@xom.io>
2024-06-11 15:40:25 -0400

    pseudo-yaml

--- enqueue.js
+++ enqueue.js
@@ -10,10 +10,76 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-const jsonFilePath = process.argv[2];
+const dataFilePath = process.argv[2];
+const dataString = fs.readFileSync(dataFilePath, 'utf8');
 
-// Read the JSON file
-const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+let jsonData = null;
+if (dataFilePath.endsWith('.json')) {
+  jsonData = JSON.parse(dataString);
+} else {
+  jsonData = {
+    permute_a: false,
+    permute_b: false,
+    a_first: true,
+    limit_consumption: true,
+    a: { physique: 0, max_physique: 0, cards: [] },
+    b: { physique: 0, max_physique: 0, cards: [] },
+  };
+  const lines = dataString.split(/\r\n|\r|\n/).map((s) => s.trim()).filter((s) => s.length);
+  let section = 0;
+  for (let line of lines) {
+    const comment = line.indexOf('#');
+    if (comment !== -1) {
+      line = line.substring(0, comment).trimEnd();
+      if (!line.length) {
+        continue;
+      }
+    }
+    if (line === 'a:') {
+      section = 1;
+      continue;
+    }
+    if (line === 'b:') {
+      section = 2;
+      continue;
+    }
+    const colon = line.indexOf(':');
+    if (colon === -1) {
+      (section === 1 ? jsonData.a : jsonData.b).cards.push(line);
+    } else {
+      const leftSide = line.substring(0, colon).trimEnd().replaceAll(' ', '_');
+      let rightSide = line.substring(colon + 1).trimStart();
+      switch (rightSide) {
+        case 'true':
+        case 'yes':
+          rightSide = true;
+          break;
+        case 'false':
+        case 'no':
+          rightSide = false;
+          break;
+        default:
+          if (!isNaN(rightSide)) {
+            rightSide = +rightSide;
+          }
+      }
+      if (section === 0) {
+        if (leftSide === 'permute') {
+          if (rightSide === 'a') {
+            jsonData.permute_a = true;
+          } else if (rightSide === 'b') {
+            jsonData.permute_b = true;
+          }
+        } else {
+          jsonData[leftSide] = rightSide;
+        }
+      } else {
+        (section === 1 ? jsonData.a : jsonData.b)[leftSide] = rightSide;
+      }
+    }
+  }
+}
+
 if (jsonData.permute_a && jsonData.permute_b) {
   console.error('Permuting both players not supported.');
   process.exit(1);
