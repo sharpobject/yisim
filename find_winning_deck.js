bb6d3ece9460766d287aba62aaf2dc0824051c0d
sharpobject <sharpobject@gmail.com>
2025-05-10 17:22:12 -0700

    add sketch of find_winning_deck for web

---
+++ find_winning_deck.js
@@ -0,0 +1,197 @@
+import {
+    swogi,
+    format_card,
+    guess_character
+} from "./gamestate_full.js";
+
+// a generator that takes an array and k and generates all k-combinations of the array's elements
+function* k_combinations(arr, k) {
+    if (k === 0) {
+        yield [];
+        return;
+    }
+    if (arr.length < k) {
+        return;
+    }
+    let head = arr[0];
+    let tail = arr.slice(1);
+    for (let subcombo of k_combinations(tail, k-1)) {
+        yield [head, ...subcombo];
+    }
+    for (let subcombo of k_combinations(tail, k)) {
+        yield subcombo;
+    }
+}
+
+function handle_response(riddle, response) {
+    const winning_decks = response.winning_decks;
+    const winning_margins = response.winning_margins;
+    const winning_logs = response.winning_logs;
+    // console.log("got response with " + winning_decks.length + " winning decks");
+    for (let i=0; i<winning_decks.length; i++) {
+        //if (winning_margins[i] > 10000) {
+        if (winning_margins[i] > riddle.best_winning_margin) {
+            riddle.best_winning_margin = winning_margins[i];
+            for (let j=0; j<winning_decks[i].length; j++) {
+                winning_decks[i][j] = format_card(winning_decks[i][j]);
+            }
+            // console.log(winning_logs[i].join("\n"));
+            //const deck_str = stringifiedd(winning_decks[i]);
+            //console.log("\"" + deck_str + "\": "+winning_margins[i]+",");
+            //deck_to_hp[deck_str] = winning_margins[i];
+            // console.log("winning deck: " + JSON.stringify(winning_decks[i]));
+            // console.log("winning margin: " + winning_margins[i]);
+        }
+    }
+    riddle.try_idx += response.try_idx;
+    // console.log("handled response");
+}
+
+async function do_riddle(riddle) {
+    const my_idx = riddle.my_idx;
+    const enemy_idx = 1 - my_idx;
+    const my_cards = riddle.players[my_idx].cards;
+    const enemy_cards = riddle.players[enemy_idx].cards;
+    riddle.best_winning_margin = -99999;
+    riddle.try_idx = 0;
+    // const preprocessor_config = {};
+    // for (let player of riddle.players) {
+    //     for (let key in player) {
+    //         preprocessor_config[key] = true;
+    //     }
+    //     for (let card_id of player.cards) {
+    //         preprocessor_config[card_id.slice(0, 5)] = true;
+    //     }
+    // }
+    // preprocess_plz(preprocessor_config);
+
+    const numCores = navigator.hardwareConcurrency;
+    const workers = [];
+    const messages_outstanding = [];
+
+    for (let i = 0; i < numCores; i++) {
+        const worker = new Worker('./web_worker.js');
+        workers.push(worker);
+        messages_outstanding.push(0);
+        // Add error event listener to each worker
+        worker.addEventListener('error', (event) => {
+            console.error(`Error in worker ${i}:`, event.message);
+        });
+    }
+
+    function createMessageHandler(workers) {
+        const messageQueue = [];
+        let resolvePromise = null;
+
+        const messageHandler = (event) => {
+            messageQueue.push(event.data);
+            if (resolvePromise) {
+                const ret = messageQueue.shift();
+                messages_outstanding[ret.worker_idx] -= 1;
+                resolvePromise(ret);
+                resolvePromise = null;
+            }
+        };
+
+        // Attach the message event listener to all workers
+        workers.forEach((worker) => {
+            worker.addEventListener('message', messageHandler);
+        });
+
+        return async function getFirstMessage() {
+            if (messageQueue.length > 0) {
+                const ret = messageQueue.shift();
+                messages_outstanding[ret.worker_idx] -= 1;
+                return ret;
+            }
+
+            return new Promise((resolve) => {
+                resolvePromise = resolve;
+            });
+        };
+    }
+    const getMessage = createMessageHandler(workers);
+
+    if (riddle.players[my_idx].character === undefined || riddle.players[my_idx].character.length !== 3) {
+        riddle.players[my_idx].character = guess_character(riddle.players[my_idx]);
+    }
+    if (riddle.players[enemy_idx].character === undefined || riddle.players[enemy_idx].character.length !== 3) {
+        riddle.players[enemy_idx].character = guess_character(riddle.players[enemy_idx]);
+    }
+    if (riddle.just_run) {
+        riddle.players[my_idx].cards = my_cards;
+        riddle.players[enemy_idx].cards = enemy_cards;
+        riddle.worker_idx = 0;
+        messages_outstanding[0] += 1;
+        workers[0].postMessage(riddle);
+    } else {
+        let combo_idx = 0;
+        const tried_combos = {};
+        for (let combo of k_combinations(my_cards, 8)) {
+            combo_idx += 1;
+            // sort the combo
+            combo.sort();
+            let combo_id = combo.join(",");
+            //if (deck_to_hp[combo_id] !== undefined) {
+            //   continue;
+            //}
+            if (tried_combos[combo_id]) {
+                continue;
+            }
+            tried_combos[combo_id] = true;
+            // if this combo has 3 or more continuous/consumption cards, skip it
+            let normal_attack_count = 0;
+            let hh_count = 0;
+            let stdiv_count = 0;
+            let faw_count = 0;
+            let stradiv_count = 0;
+            let twice_count = 0;
+            let concon_count = 0;
+            for (let i=0; i<combo.length; i++) {
+                if (swogi[combo[i]].is_continuous || swogi[combo[i]].is_consumption) {
+                    concon_count += 1;
+                }
+            }
+            if (concon_count >= 3) {
+                continue;
+            }
+            // console.log("combo_idx: " + combo_idx);
+            // console.log("concon_count: " + concon_count);
+            // if there is a ready worker, send the message
+            let worker_idx = -1;
+            for (let i=0; i<numCores; i++) {
+                if (messages_outstanding[i] == 0) {
+                    worker_idx = i;
+                    break;
+                }
+            }
+            if (worker_idx == -1) {
+                // wait for a message to come back
+                let response = await getMessage();
+                worker_idx = response.worker_idx;
+                handle_response(riddle, response);
+            }
+            riddle.players[my_idx].cards = combo;
+            riddle.worker_idx = worker_idx;
+            //console.log("sending message to worker_idx: " + worker_idx);
+            messages_outstanding[worker_idx] += 1;
+            workers[worker_idx].postMessage(riddle);
+        }
+    }
+    // wait for all messages to come back
+    let total_messages_outstanding = 0;
+    for (let i=0; i<numCores; i++) {
+        total_messages_outstanding += messages_outstanding[i];
+    }
+    // console.log("total_messages_outstanding: " + total_messages_outstanding);
+    while (total_messages_outstanding > 0) {
+        let response = await getMessage();
+        handle_response(riddle, response);
+        total_messages_outstanding -= 1;
+    }
+    // shut down the workers
+    for (let i = 0; i < numCores; i++) {
+        workers[i].terminate();
+    }
+    // console.log("try_idx: " + riddle.try_idx);
+}
---
+++ web_worker.js
@@ -0,0 +1,180 @@
+// First import
+importScripts('./gamestate_full.js');
+
+// Extract what you need from that import
+const GameStateWithLog = self.GameStateWithLog;
+const PlayerWithLog = self.PlayerWithLog;
+
+// Second import (might overwrite some globals)
+importScripts('./gamestate_full_nolog.js');
+
+// Extract what you need from the second import
+const GameState = self.GameState;
+const Player = self.Player;
+const card_name_to_id_fuzzy = self.card_name_to_id_fuzzy;
+
+
+function next_permutation(arr) {
+    let i = arr.length - 1;
+    while (i > 0 && arr[i-1] >= arr[i]) {
+        i -= 1;
+    }
+    if (i === 0) {
+        return false;
+    }
+    let j = arr.length - 1;
+    while (arr[j] <= arr[i-1]) {
+        j -= 1;
+    }
+    let temp = arr[i-1];
+    arr[i-1] = arr[j];
+    arr[j] = temp;
+    j = arr.length - 1;
+    while (i < j) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i += 1;
+        j -= 1;
+    }
+    return true;
+}
+
+// receive a GameState and return a list of most-winning decks
+onmessage = (event) => {
+// export const onmessage = (event) => {
+    const worker_idx = event.data.worker_idx;
+    const players = event.data.players;
+    const my_idx = event.data.my_idx;
+    const enemy_idx = 1 - my_idx;
+    const combo = event.data.players[my_idx].cards.slice();
+    const ret = {};
+    const winning_decks = [];
+    const winning_margins = [];
+    const winning_logs = [];
+    let try_idx = 0;
+    let best_winning_margin = -99999;
+    let best_winrate = 0.8;
+    let best_hp = -99999;
+    const a = new Player();
+    const b = new Player();
+    const alog = new PlayerWithLog();
+    const blog = new PlayerWithLog();
+    if (event.data.just_run) {
+        let game = new GameStateWithLog(alog, blog);
+        for (let key in players[my_idx]) {
+            game.players[my_idx][key] = players[my_idx][key];
+        }
+        for (let key in players[enemy_idx]) {
+            game.players[enemy_idx][key] = players[enemy_idx][key];
+        }
+        game.players[my_idx].cards = combo;
+        if (event.data.zongzi) {
+            game.sim_n_turns_zongzi(8);
+        } else if (event.data.dummy) {
+            game.sim_n_turns_zongzi(12);
+        } else {
+            game.sim_n_turns(64);
+        }
+        const winning_margin = game.players[my_idx].hp - game.players[enemy_idx].hp - 1000 * game.turns_taken;
+        const p_combo = combo.slice();
+        winning_decks.push(p_combo);
+        winning_margins.push(winning_margin);
+        winning_logs.push(game.output);
+    } else {
+        do {
+            var winrate = 0;
+            try_idx += 1;
+            //console.log("try_idx: " + try_idx);
+            //os.exit(1);
+            let game = new GameState(a, b);
+            for (let key in players[my_idx]) {
+                game.players[my_idx][key] = players[my_idx][key];
+            }
+            for (let key in players[enemy_idx]) {
+                game.players[enemy_idx][key] = players[enemy_idx][key];
+            }
+            game.players[my_idx].cards = combo;
+            if (event.data.zongzi) {
+                game.sim_n_turns_zongzi(8);
+            } else if (event.data.dummy) {
+                game.sim_n_turns_zongzi(12);
+            } else {
+                game.sim_n_turns(64);
+            }
+            
+            if (game.used_randomness && game.winner === my_idx && false) {
+                var win_count = game.winner === my_idx ? 1 : 0;
+                var total_count = 1;
+                for(var qq=0; qq<20; qq++) {
+                    let game = new GameState(a, b);
+                    for (let key in players[my_idx]) {
+                        game.players[my_idx][key] = players[my_idx][key];
+                    }
+                    for (let key in players[enemy_idx]) {
+                        game.players[enemy_idx][key] = players[enemy_idx][key];
+                    }
+                    game.players[my_idx].cards = combo;
+                    game.sim_n_turns(64);
+                    total_count += 1;
+                    if (game.winner === my_idx) {
+                        win_count += 1;
+                    }
+                }
+                winrate = win_count / total_count;
+            } else {
+                winrate = game.winner === my_idx ? 1 : 0;
+            }
+            if (game.winner === my_idx && !game.used_randomness) {
+            //if (game.players[enemy_idx].hp > 8599 && !game.used_randomness) {
+            //if (game.winner === my_idx && !game.used_randomness) {
+            //if (combo[0] == "633011" && combo[2] == "135072") {
+            //if (!game.used_randomness) {
+            //if (winrate >= best_winrate) {
+                best_winrate = winrate;
+                const hp = game.players[my_idx].hp;
+                //if (hp > best_hp) {
+                //    winning_decks.length = 0;
+                //    winning_margins.length = 0;
+                //    winning_logs.length = 0;
+                //}
+                //best_hp = hp;
+                //const winning_margin = game.players[my_idx].hp - game.players[enemy_idx].hp - 1000 * game.turns_taken;
+                const winning_margin = -game.players[enemy_idx].hp;
+                const p_combo = combo.slice();
+                if (winning_margin > best_winning_margin) {
+                    winning_decks.length = 0;
+                    winning_margins.length = 0;
+                    winning_logs.length = 0;
+                    best_winning_margin = winning_margin;
+                    winning_decks.push(p_combo);
+                    winning_margins.push(winning_margin);
+                    let game_with_log = new GameStateWithLog(alog, blog);
+                    for (let key in players[my_idx]) {
+                        game_with_log.players[my_idx][key] = players[my_idx][key];
+                    }
+                    for (let key in players[enemy_idx]) {
+                        game_with_log.players[enemy_idx][key] = players[enemy_idx][key];
+                    }
+                    game_with_log.players[my_idx].cards = combo;
+                    if (event.data.zongzi) {
+                        game_with_log.sim_n_turns_zongzi(8);
+                    } else if (event.data.dummy) {
+                        game_with_log.sim_n_turns_zongzi(12);
+                    } else {
+                        game_with_log.sim_n_turns(64);
+                    }
+                    winning_logs.push(game_with_log.output);
+                }
+            }
+        } while (next_permutation(combo));
+    }
+    ret.worker_idx = worker_idx;
+    ret.combo = combo;
+    ret.winning_decks = winning_decks;
+    ret.winning_margins = winning_margins;
+    ret.winning_logs = winning_logs;
+    ret.try_idx = try_idx;
+    postMessage(ret);
+    //return ret;
+};
