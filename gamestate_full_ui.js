a8392ad7f17b261019706effb5dca338004c2e18
Kiven <412386861@qq.com>
2025-06-02 00:33:43 +0800

    fix do_set_cards_by_round

--- gamestate_full_ui.js
+++ gamestate_full_ui.js
@@ -706,7 +706,7 @@ export class Player {
         this.cards = this.cards.slice(0, slot);
     }
     reset_can_play() {
-        do_set_cards_by_round()
+        this.do_set_cards_by_round()
         this.can_play.length = 0;
         this.is_star_point.length = 0;
         this.can_post_action.length = 0;
@@ -1060,15 +1060,6 @@ export class GameState {
             }
         }
     }
-    do_set_cards_by_round(idx) {
-        const me = this.players[idx];
-        let slot = 8;
-        if (me.round_number) {
-            slot = Math.min(8, me.round_number + 2);
-        }
-        me.hand_cards = me.cards.slice(slot);
-        me.cards = me.cards.slice(0, slot);
-    }
     do_the_body_of_fierce_tiger(idx) {
         const me = this.players[idx];
         if (me.the_body_of_fierce_tiger_stacks === 0) {
@@ -1144,7 +1135,6 @@ export class GameState {
             this.players[idx].post_deck_setup();
         }
         for (let idx = 0; idx < 2; idx++) {
-            this.do_set_cards_by_round(idx);
             this.do_the_body_of_fierce_tiger(idx);
             this.do_pact_of_equilibrium(idx);
             this.do_pact_of_adversity_reinforcement(idx);
